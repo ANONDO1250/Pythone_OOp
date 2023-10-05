c7110dcc4d85969d0ba760dce19e76eda42fcdd3
Mehedi Hasan <Mohammadmehedi1250@gmail.com>
2023-10-05 23:17:57 +0600

    commit

---
+++ Basic_Python/information.py
@@ -0,0 +1,9 @@
+name = "Mehedi Hasan Anondo"
+age = 21
+home_town = "B-Baria"
+Present_Address = "Narshingdi"
+
+print(name)
+print(age)
+print(home_town)
+print(Present_Address)
---
+++ Basic_Python/input.py
@@ -0,0 +1,9 @@
+print("Now I Need Money")
+# input("Give me some Money : ")
+# money = input("Give me some Money : ")
+# print(money)
+# print("Here is Your money ", money)
+f_Money = "Mehedi Kisu taka de : "
+
+l_money = "Lubna kisu Taka deo : "
+print(f_Money, l_money)
